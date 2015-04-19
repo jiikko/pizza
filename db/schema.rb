a90fa2af799a178d8a1ae7afd3e3c96aef6c5744
jiikko <n905i.1214@gmail.com>
2015-04-19 10:28:53 +0900

    退屈です

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150419005003) do
+ActiveRecord::Schema.define(version: 20150419012825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20150419005003) do
   create_table "operations", force: :cascade do |t|
     t.string   "name"
     t.text     "script"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "scenarios", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
