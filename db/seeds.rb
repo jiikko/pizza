9caa1b54e8eba2cdbbffdc74f97559a7eff69cce
jiikko <n905i.1214@gmail.com>
2015-04-20 19:45:16 +0900

    ああああああああ

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Operation.destroy_all
+
+op = Operation.new(name: "すごいよ")
+op.tag_list = "テスト, ジャバスクリプトだ, そういうやつ"
+op.save
+
+op = Operation.new(name: "花粉症なの? ")
+op.tag_list = "ラッパー, ジャバスクリプトだ, ネコ"
+op.save
+
+10.times do
+  op = Operation.new(name: "誰 ")
+  op.tag_list = "ラッパー, ジャバスクリプトだ, ネコ"
+  op.save
+end
