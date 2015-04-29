fe3a2f25902451783644ead51ce3c3490dc03670
jiikko <n905i.1214@gmail.com>
2015-04-29 21:58:32 +0900

    未登録なページを踏んでも何もおきないようにする

--- chrome_extenction/javascripts/app.js
+++ chrome_extenction/javascripts/app.js
@@ -2,6 +2,7 @@
 // memo http://stackoverflow.com/questions/3857874/how-to-dynamically-insert-a-script-tag-via-jquery-after-page-load
 
 $.getJSON("http://localhost:3000/operations/search?url=" + document.URL, function(record){
+  if(record === null) { console.log("not found target_url"); return; }
   $.getJSON("http://localhost:3000/operations/" + record.id + ".json", function(json){
         var script_tag = $("<script>");
         script_tag.html(json.script);
