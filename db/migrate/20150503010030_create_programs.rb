370b0262d33d2a0486e09392ed23a90a2758759a
jiikko <n905i.1214@gmail.com>
2015-05-03 10:02:27 +0900

    add model

---
+++ db/migrate/20150503010030_create_programs.rb
@@ -0,0 +1,10 @@
+class CreatePrograms < ActiveRecord::Migration
+  def change
+    create_table :programs do |t|
+      t.text :code
+      t.references :programing_language, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150502015740) do
+ActiveRecord::Schema.define(version: 20150502054504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 20150502015740) do
     t.text     "script"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "programing_snippets", force: :cascade do |t|
+    t.text     "code"
+    t.integer  "programing_language_id", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "scenario_operations", force: :cascade do |t|
