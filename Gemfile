23843fb7b75c530673d9ca59d95c10f2e926a800
jiikko <n905i.1214@gmail.com>
2015-05-14 17:16:36 +0900

    google認証の準備

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '4.2.1'
-gem 'pg'
+gem 'mysql2'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
@@ -15,6 +15,7 @@ gem "twitter-bootstrap-rails"
 gem 'acts-as-taggable-on', '~> 3.4.2'
 gem 'sugoi_selectbox-rails'
 gem 'nested_form'
+gem 'sorcery'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
--- db/schema.rb
+++ db/schema.rb
@@ -11,40 +11,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150503010536) do
+ActiveRecord::Schema.define(version: 20150514075205) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  create_table "authentications", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4,   null: false
+    t.string   "provider",   limit: 255, null: false
+    t.string   "uid",        limit: 255, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "authentications", ["provider", "uid"], name: "index_authentications_on_provider_and_uid", using: :btree
 
   create_table "operations", force: :cascade do |t|
-    t.string   "name"
-    t.boolean  "enable",     default: false, null: false
-    t.string   "target_url"
-    t.text     "script"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+    t.string   "name",       limit: 255
+    t.boolean  "enable",     limit: 1,     default: false, null: false
+    t.string   "target_url", limit: 255
+    t.text     "script",     limit: 65535
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
   end
 
   create_table "programing_languages", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "programs", force: :cascade do |t|
-    t.text     "code"
-    t.integer  "programing_language_id", null: false
-    t.integer  "operation_id",           null: false
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.text     "code",                   limit: 65535
+    t.integer  "programing_language_id", limit: 4,     null: false
+    t.integer  "operation_id",           limit: 4,     null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
   end
 
   add_index "programs", ["operation_id"], name: "index_programs_on_operation_id", using: :btree
 
   create_table "scenario_operations", force: :cascade do |t|
-    t.integer "scenario_id",              null: false
-    t.integer "operation_id",             null: false
-    t.integer "position",     default: 0, null: false
+    t.integer "scenario_id",  limit: 4,             null: false
+    t.integer "operation_id", limit: 4,             null: false
+    t.integer "position",     limit: 4, default: 0, null: false
   end
 
   add_index "scenario_operations", ["operation_id", "scenario_id"], name: "index_scenario_operations_on_operation_id_and_scenario_id", using: :btree
@@ -53,17 +60,17 @@ ActiveRecord::Schema.define(version: 20150503010536) do
   add_index "scenario_operations", ["scenario_id"], name: "index_scenario_operations_on_scenario_id", using: :btree
 
   create_table "scenarios", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "taggings", force: :cascade do |t|
-    t.integer  "tag_id"
-    t.integer  "taggable_id"
-    t.string   "taggable_type"
-    t.integer  "tagger_id"
-    t.string   "tagger_type"
+    t.integer  "tag_id",        limit: 4
+    t.integer  "taggable_id",   limit: 4
+    t.string   "taggable_type", limit: 255
+    t.integer  "tagger_id",     limit: 4
+    t.string   "tagger_type",   limit: 255
     t.string   "context",       limit: 128
     t.datetime "created_at"
   end
@@ -71,10 +78,23 @@ ActiveRecord::Schema.define(version: 20150503010536) do
   add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true, using: :btree
 
   create_table "tags", force: :cascade do |t|
-    t.string  "name"
-    t.integer "taggings_count", default: 0
+    t.string  "name",           limit: 255
+    t.integer "taggings_count", limit: 4,   default: 0
   end
 
   add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                        limit: 255, null: false
+    t.string   "crypted_password",             limit: 255
+    t.string   "salt",                         limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "remember_me_token",            limit: 255
+    t.datetime "remember_me_token_expires_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["remember_me_token"], name: "index_users_on_remember_me_token", using: :btree
 
 end
